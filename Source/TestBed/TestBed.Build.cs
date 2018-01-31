63efa311c69c8d3b6156f937a29084b63e099bca
Ash Matheson <nuclearfossil@hotmail.com>
2018-01-30 21:39:06 -0800

    Update to 4.16.3

--- Source/TestBed/TestBed.Build.cs
+++ Source/TestBed/TestBed.Build.cs
@@ -4,18 +4,25 @@ using UnrealBuildTool;
 
 public class TestBed : ModuleRules
 {
-	public TestBed(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public TestBed(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
-        // PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });                
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 
         // Increase build time?
-        // MinFilesUsingPrecompiledHeaderOverride = 1;
-        // bFasterWithoutUnity = true;
+        MinFilesUsingPrecompiledHeaderOverride = 1;
+        bFasterWithoutUnity = true;
 
-		// Remember to disable this once we want to ship.
-		// OptimizeCode = CodeOptimization.Never;
-	}
+        // Remember to disable this once we want to ship.
+        OptimizeCode = CodeOptimization.Never;
+    }
 }
